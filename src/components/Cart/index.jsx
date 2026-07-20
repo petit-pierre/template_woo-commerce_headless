1e05afe26e371568d47a627115e5d7cfcee1e646
cosmin-bilga <cosmin.bilga@laplateforme.io>
2026-07-20 10:17:03 +0200

    CartProduct Component

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,72 +1,20 @@
-import { useSelector, useDispatch } from "react-redux";
-import {
-  addProductToCart,
-  deleteProductFromCart,
-  substractProductFromCart,
-} from "../../thunkActionsCreator/cartThunks";
+import { useSelector } from "react-redux";
+import { CartProduct } from "./CartProduct";
 
 export default function Cart() {
   const items = useSelector((state) => state.cart.items);
   const totals = useSelector((state) => state.cart.totals);
-  const dispatch = useDispatch();
-
-  console.log(items);
 
   return (
     <>
       <div>Votre Panier</div>
       <ul>
         {items.map((item) => (
-          <li style={{ display: "flex", gap: "10px" }}>
-            <img
-              src={item.images[0].thumbnail}
-              style={{ width: "100px", height: "100px" }}
-            ></img>
-            <p>{item.name}</p>
-            <span
-              dangerouslySetInnerHTML={{ __html: item.short_description }}
-            ></span>
-            <p>{item.quantity}</p>
-            <button
-              onClick={() => {
-                dispatch(
-                  addProductToCart({
-                    productId: item.id,
-                    quantity: 1,
-                    variation: item.variation,
-                  }),
-                );
-              }}
-            >
-              Ajouter +
-            </button>
-            <button
-              onClick={() => {
-                dispatch(
-                  substractProductFromCart({
-                    itemKey: item.key,
-                    quantity: item.quantity,
-                  }),
-                );
-              }}
-            >
-              Reduire -
-            </button>
-
-            <button
-              onClick={() => {
-                dispatch(
-                  deleteProductFromCart({
-                    itemKey: item.key,
-                  }),
-                );
-              }}
-            >
-              Supprimer
-            </button>
-          </li>
+          <CartProduct key={item.key} item={item} />
         ))}
       </ul>
+      <button>Vider Panier</button>
+      <button>Payer</button>
     </>
   );
 }
