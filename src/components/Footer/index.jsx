1c25577b031a037ebe066b06e29b22caca44cfcd
Rsoumre <harounasoumare547@gmail.com>
2026-07-22 14:33:21 +0200

    update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,18 +9,16 @@ export default function Footer() {
       <div className="footer_grid">
 
         <div className="footer_col">
-          <h6 className="footer_col-title">À propos</h6>
           <ul className="footer_links">
             <li><Link to="/catalogue">Acceuil</Link></li>
             <li><Link to="/catalogue">Catalogue</Link></li>
             <li><Link to="/cart">Panier</Link></li>
+            <li><Link to="/login">Se connecter</Link></li>
           </ul>
         </div>
 
         <div className="footer_col">
-          <h6 className="footer_col-title">Besoin d'aide</h6>
           <ul className="footer_links">
-            <li><Link to="/catalogue">Catalogue</Link></li>
             <li><Link to="/cgu">Conditions générales d'utilisation</Link></li>
             <li><Link to="/cgv">Conditions générales de vente</Link></li>
             <li><Link to="/mentions-legales">Mentions légales</Link></li>
@@ -31,9 +29,7 @@ export default function Footer() {
 
       </div>
 
-      <div className="footer_bottom">
-        <p>© {new Date().getFullYear()} Méo. Tous droits réservés.</p>
-      </div>
+
     </footer>
   );
 }
