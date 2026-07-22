0882a36cc35b413cc9c9d8a01c987f82c98502bd
petit-pierre <aubree.pierre2@gmail.com>
2026-07-22 16:23:50 +0200

    V2.0.2 slug cgv

--- src/pages/CGV/index.jsx
+++ src/pages/CGV/index.jsx
@@ -1,5 +1,5 @@
 import PageContent from "../../components/PageContent";
 
 export default function CGV() {
-  return  <PageContent slug="conditions-generales-de-vente" />;
+  return <PageContent slug="conditions-generales-de-vente-cgv" />;
 }
